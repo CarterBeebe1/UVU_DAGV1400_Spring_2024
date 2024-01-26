8b95a99db23d192a8fb15b8e976b3c7078138450
Carter Beebe <155788039+CarterBeebe1@users.noreply.github.com>
2024-01-26 00:50:53 -0700

    Added bool condition

--- Unit 1a - Lab.cs
+++ Unit 1a - Lab.cs
@@ -1,5 +1,6 @@
-// This program calculates the total number of packages of flour from an
-// initial amount of wheat grown in yards.
+// The program calculates the total number of packages of flour from an
+// initial amount of wheat grown in yards. It then indicates whether
+// there are enough packages for shipment.
 
 using System;
 
@@ -7,17 +8,36 @@ public class WheatMill
 {
 	public static void Main()
 	{
-		int yardsOfWheat = 3456;	// Number of yards of wheat farmed.
+		int yardsOfWheat = 3486;		// Number of yards of wheat farmed.
 		double poundsPerYard = 0.137D;	// Pounds of wheat per yard.
-		int poundsPerPack = 4;		// Number of pounds in a package of wheat.
+		int poundsPerPack = 4;			// Number of pounds in a package of wheat.
+		bool shipment = false;			// Indicates whether there are enough packages for shipment.
 		
-		double yeild = yardsOfWheat * poundsPerYard;		// Calculate total yeild of wheat in pounds by multiplying yards of wheat by the amount of pounds each yard yeilds.
+		double yeild = yardsOfWheat * poundsPerYard;			// Calculate total yeild of wheat in pounds by multiplying yards of wheat by the amount of pounds each yard yeilds.
 		double packageAmt = Math.Round(yeild / poundsPerPack);	// Find total possible packages of wheat by dividing the total pounds
-									// of grain by the amount of pounds in each package and discardsthe  decimal value.
-        	string text = "Total number of packages: "; 		// String of text to output to Console.
-		
+																// of grain by the amount of pounds in each package and discardsthe  decimal value.
+		string text = "Total number of packages: ";
+
 
 		// Output the total number of packages.
+
 		Console.WriteLine(text + packageAmt);
+
+		
+		// Tests whether there are enough packages for shipment and output text indicating whether packages are ready to be shipped.
+		
+		if (packageAmt >= 100) 
+      	{
+        	shipment = true;
+      	}
+		
+		if (shipment)
+		{
+			Console.WriteLine("There are enough packages for shipment");
+		}
+		else
+		{
+			Console.WriteLine("There are not enough packages for shipment");
+		}
 	}
 }
