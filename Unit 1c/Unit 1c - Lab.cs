74dc80f4a92b652f67b5dd255ee09aa8964f6e4c
GrassPlanet <carterlbeebe@gmail.com>
2024-02-27 22:50:31 -0700

    Created program for Unit 1c - Lab. Cleaned up code.

---
+++ Unit 1c/Unit 1c - Lab.cs
@@ -0,0 +1,28 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		// Asks user to enter a positive integer
+		Console.WriteLine("Enter a positive integer:");
+		var input = Console.ReadLine();
+		
+		// Converts user input to an integer
+		int num = Convert.ToInt32(input);
+		
+		// Adds a space
+		Console.WriteLine(" ");
+		
+		// Nested for loop. The outside loop runs until i is greater than or equal to num
+		// The inside loop outputs i to the console i number of times
+		for (var i = 1; i <= num; i++)
+		{
+			for (var w = 1; w <= i; w++)
+			{
+				Console.Write(i);
+			}
+			Console.WriteLine("");
+		}
+	}
+}
