8acf3db251a5d616325fd617168b486264cdfcce
Carter Beebe <155788039+CarterBeebe1@users.noreply.github.com>
2024-02-29 22:16:09 -0700

    Created FavoriteFoods function

--- Unit 1c/Unit 1c - Challenge.cs
+++ Unit 1c/Unit 1c - Challenge.cs
@@ -1,41 +1,34 @@
-﻿using System;
+using System;
 					
 public class Program
 {
 	public static void Main()
 	{
-		// Generate a random number between 1 and 10
-		// Ask user to guess the number and save user's input to a variable
-		int randomNumber = new Random().Next(1, 11);
+		string[] foods = {"Food1", "Food2", "Food3"};
 		
-		// Try converting user's input to an int. Output "Invalid input" if user did not enter an integer
-		//bool success = int.TryParse(input, out num);
-
-		//if (success == false)
-		//{
-		//	Console.WriteLine("Invalid input");
-		//}
+		Console.WriteLine("Enter your three favorite foods:");
 		
-		bool guessedNum = false;
-		
-		Console.WriteLine("Guess the random number between 1 and 10:");
-		
-		while (guessedNum == false)
+		for(var i = 1; i <= 3; i++)
 		{
-			var input = Console.ReadLine();
-			int guess = Convert.ToInt32(input);
-			
-			if (guess > randomNumber)
-			{
-				Console.WriteLine("Too high.");
-			}
-			else if (guess < randomNumber){
-				Console.WriteLine("Too low.");
-			}
-			else
-			{
-				Console.WriteLine("You guessed the correct number. Well done!");
-			}
+			Console.Write(i + ": ");
+			foods[i-1] = Console.ReadLine();
 		}
+
+		Console.WriteLine(foods);
+	}
+	
+	public static void FavoriteFoods()
+	{
+		string[] foods = {"Food1", "Food2", "Food3"};
+		
+		Console.WriteLine("Enter your three favorite foods:");
+		
+		for(var i = 1; i <= 3; i++)
+		{
+			Console.Write(i + ": ");
+			foods[i-1] = Console.ReadLine();
+		}
+
+		Console.WriteLine(foods);
 	}
 }
