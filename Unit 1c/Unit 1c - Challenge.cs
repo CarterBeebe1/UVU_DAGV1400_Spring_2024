61dcd1eb34a445de83a217188dbcbf17ea4a6c9c
GrassPlanet <carterlbeebe@gmail.com>
2024-02-29 19:45:31 -0700

    Created "while" Loop to check user input

--- Unit 1c/Unit 1c - Challenge.cs
+++ Unit 1c/Unit 1c - Challenge.cs
@@ -7,16 +7,35 @@ public class Program
 		// Generate a random number between 1 and 10
 		// Ask user to guess the number and save user's input to a variable
 		int randomNumber = new Random().Next(1, 11);
-		Console.WriteLine("Guese the random number between 1 and 10:");
-		var input = Console.ReadLine();
 		
 		// Try converting user's input to an int. Output "Invalid input" if user did not enter an integer
-		int num;
-		bool success = int.TryParse(input, out num);
+		//bool success = int.TryParse(input, out num);
 
-		if (success == false)
+		//if (success == false)
+		//{
+		//	Console.WriteLine("Invalid input");
+		//}
+		
+		bool guessedNum = false;
+		
+		Console.WriteLine("Guess the random number between 1 and 10:");
+		
+		while (guessedNum == false)
 		{
-		Console.WriteLine("Invalid input");
+			var input = Console.ReadLine();
+			int guess = Convert.ToInt32(input);
+			
+			if (guess > randomNumber)
+			{
+				Console.WriteLine("Too high.");
+			}
+			else if (guess < randomNumber){
+				Console.WriteLine("Too low.");
+			}
+			else
+			{
+				Console.WriteLine("You guessed the correct number. Well done!");
+			}
 		}
 	}
 }
