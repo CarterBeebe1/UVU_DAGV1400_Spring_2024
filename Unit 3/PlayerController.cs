67bd5a083b6fc4dfdf4adc21d1be02538078c943
GrassPlanet <carterlbeebe@gmail.com>
2024-04-10 12:53:45 -0600

    Added Player Controller script

---
+++ Unit 3/PlayerController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Variable declaration
+    public float movementSpeed = 5f;
+    public float jumpForce = 5f;
+    private float gravity = 9.81f;
+
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    private bool isJumping;
+    private bool grounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get player input for horizontal and vertical movment
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Move player at move speed based on vertical and horizontal input
+        controller.Move(Vector3.forward * verticalInput * movementSpeed * Time.deltaTime);
+        controller.Move(Vector3.right * horizontalInput * movementSpeed * Time.deltaTime);
+
+        // Check if player is grounded and the jump key is pressed
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+
+        // Apply jump force if true
+        if (isJumping && grounded)
+        {
+            moveDirection.y = jumpForce;
+            isJumping = false;
+            grounded = false;
+        }
+
+        // Apply gravity
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
+        // Reset moveDirection to zero if character is grounded
+        if (controller.isGrounded)
+        {
+            moveDirection.y = 0;
+            grounded = true;
+        }
+    }
+}
