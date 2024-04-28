250b5fb838b3b0626a13613e42c6f2acfe1a1a4e
GrassPlanet <carterlbeebe@gmail.com>
2024-04-27 22:06:57 -0600

    Completed all tasks and bonus tasks for challenge 4b - Play Fetch

---
+++ Unit 4b/Play Fetch/Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public bool previousDog = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!previousDog)
+            {
+                StartCoroutine(DogSpawner());
+            }
+        }
+    }
+    IEnumerator DogSpawner()
+    {
+        previousDog = true;
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        yield return new WaitForSeconds(0.3f);
+        previousDog = false;
+        
+    }
+}
---
+++ Unit 4b/Play Fetch/Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnTimer;
+    private int spawnIntervalLower = 1;
+    private int spawnIntervaUpper = 5;
+    private float currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTimer = startDelay;
+        currentTime = Time.time;
+    }
+
+    private void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+
+        if (spawnTimer <= 0)
+        {
+            SpawnRandomBall();
+            spawnTimer = Random.Range(spawnIntervalLower, spawnIntervaUpper + 1);
+        }
+    }
+
+    void SpawnRandomBall()
+    {
+        int randomNum = UnityEngine.Random.Range(0, ballPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        Instantiate(ballPrefabs[randomNum], spawnPos, ballPrefabs[randomNum].transform.rotation);
+    }
+}
